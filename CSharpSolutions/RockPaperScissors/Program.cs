5b15519c2616f6cc30b3b7e5783b6a5daa30d68a
ericcoker18 <ericcoker18@Yahoo.com>
2021-02-06 19:26:01 -0600

    RPS

---
+++ CSharpSolutions/RockPaperScissors/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Choices = { "rock", "paper", "scissors" };
+            Console.WriteLine("Pick Rock, Paper, or Scissors");
+            string Pick = Console.ReadLine().ToLower();
+            Random CPU = new Random();
+            int RandomNumb = CPU.Next(0, 3);
+            string cpu = Choices[RandomNumb];
+            
+            if (Pick == cpu)
+            {
+                Console.WriteLine("Tie");
+            }
+
+            else if (Pick == "rock")
+            {
+                if (cpu == "paper")
+                {
+                    Console.WriteLine("Cpu Wins!");
+                }
+                else if (cpu == "scissors")
+                {
+                    Console.WriteLine("Player wins!");
+                }
+            }
+            else if (Pick == "paper")
+            {
+               if (cpu == "rock")
+                {
+                    Console.WriteLine("Player wins!");
+                }
+               else if (cpu == "scissors")
+                {
+                    Console.WriteLine("Cpu Wins!");
+                }
+                    
+            }   
+            else if (Pick == "scissors")
+            {
+                if (cpu == "rock")
+                {
+                    Console.WriteLine("Cpu Wins!");
+                }
+                else if (cpu == "paper")
+                {
+                    Console.WriteLine("Player Wins!");
+                }
+                
+
+
+                 
+            }
+            else { Console.WriteLine("You did this wrong"); }
+            
+
+
+          
+        }
+    }
+}
