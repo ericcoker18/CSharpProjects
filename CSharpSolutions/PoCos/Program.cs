aef2e1a0f507cc28eb46401242ba6bee7b7e4695
ericcoker18 <ericcoker18@Yahoo.com>
2021-02-08 20:32:05 -0600

    PoCos

---
+++ CSharpSolutions/PoCos/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PoCos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
